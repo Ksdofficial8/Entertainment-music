5d67556aadaa7392fe01e7af23881f1f7c1a3ec7
Ksdofficial8 <ksdofficial84@gmail.com>
2024-12-25 12:57:28 +0530

    Update Musarrat.py

--- Musarrat.py
+++ Musarrat.py
@@ -86,7 +86,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7341956587:AAEq3IeuEBtPX48ar5iBbU26mYAwfC6wMl0"
 STRING_SESSION = getenv("STRING_SESSION", "BQGV228ACV8QkqqOPePVWjymPlMQa70ISY-ZH4UKxNRi-wsV7nLeNum4mHf5E4moYwdAJ8QBCP6DgJ1XGfdA-J86nsBjgBcTIvBYwaUF0nC0IcqTtjQwZb14H_YCAKzYUyA3HbUP8kESK8cIAJh1w6T-KorFff_WmVECStrrM-Hu8K24Zbg--p4FqNac1lUsjOaQsZP1OX2sU46CDJvg74k-s-3B8-xOd0HRYDo57j4rcv-UgB_OXOSi2-exw-Ma-CaDQrxpnzaHh-DhFN1y04-Bc4-5bukKQoTKJ29oEf9DSjpwFsSPB00u7ekXMobWZjQfxa17_6Kro_AgpzrzO1dS7xqvRAAAAAF7cTpGAA")
 MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Yash_607:Yash_607@cluster0.r3s9sbo.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "8009369344"))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "--1002464403548"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002464403548"))
 START_IMAGE_URL = getenv("START_IMAGE_URL", "https://files.catbox.moe/24z6cw.mp4")
 REPO_IMAGE_URL = getenv("REPO_IMAGE_URL", "https://files.catbox.moe/nswh7s.jpg")
 STATS_IMAGE_URL = getenv("STATS_IMAGE_URL", "https://files.catbox.moe/2hgoq7.jpg")
